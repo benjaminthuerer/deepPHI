2e451fd87bd6a48dc6a0e3abf2c05ac968d9ad7d
Benjamin Thürer <benjat@uio.no>
2019-06-12 14:28:14 +0200

    load mat data

--- index.py
+++ index.py
@@ -1,6 +1,12 @@
-import numpy as np
+import scipy.io
 
-print("hello world")
+data_path = 'Z:/13_deepPhi/'
+files_train = ['conn_matrices.mat', 'network_matrices.mat', 'phismax.mat', 'phismeans.mat']
 
-print("see what happens now")
+conn_matrices = network_matrices = phismax = phismeans = []
 
+for file in files_train:
+    vars()[file[:-4]] = scipy.io.loadmat(data_path + file)['mat']
+
+phismax = phismax.tolist()[0]
+phismeans = phismeans.tolist()[0]
